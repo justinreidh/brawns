e3063649d975858c0b899b83355f87cbb0b8e49d
justinreidh <justintheratchethoe@gmail.com>
2025-03-26 19:28:06 -0600

    added dummy pages

---
+++ app/scores/page.js
@@ -0,0 +1,13 @@
+export default function ScorePage() {
+    return (
+        <div>
+            <h1>Your scores:</h1>
+            <ul>
+                <li>Dummy Score</li>
+                <li>Dummy Score</li>
+                <li>Dummy Score</li>
+                <li>Dummy Score</li>
+            </ul>
+        </div>
+    )
+}
